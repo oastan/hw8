30a4520cd173b5c504ad6eb87247a3842735fc6c
Oleg <oastan@ya.ru>
2024-07-15 06:28:01 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("task 1");
-        int[] one = new int [3];
+        int[] one = new int[3];
         one[0] = 1;
         one[1] = 2;
         one[2] = 3;
 
-        double [] two = {1.57, 7.654, 9.986};
+        double[] two = {1.57, 7.654, 9.986};
 
-        int [] three = {1, 2, 3, 4, 5, 6, 7};
+        int[] three = {1, 2, 3, 4, 5, 6, 7};
 
         System.out.println("task 2");
 
@@ -42,21 +42,21 @@ public class Main {
         }
 
         System.out.println("task 3");
-        for(int b = one.length -1; b>=0; b--) {
+        for (int b = one.length - 1; b >= 0; b--) {
             System.out.print(one[b]);
             if (b != 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for(int b = two.length -1; b>=0; b--) {
+        for (int b = two.length - 1; b >= 0; b--) {
             System.out.print(two[b]);
             if (b != 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for(int b = three.length -1; b>=0; b--) {
+        for (int b = three.length - 1; b >= 0; b--) {
             System.out.print(three[b]);
             if (b != 0) {
                 System.out.print(", ");
@@ -67,40 +67,11 @@ public class Main {
         System.out.println("task 4");
 
         for (int c = 0; c < one.length; c++) {
-            if (one[c] + 2 != 0) {
+            if (one[c] % 2 != 0) {
                 one[c] += 1;
             }
         }
         System.out.print(Arrays.toString(one));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
